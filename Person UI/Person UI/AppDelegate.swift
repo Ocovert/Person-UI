83f753974e9dea42c5661b815781a5fc4cff4743
Ocovert <42614487+Ocovert@users.noreply.github.com>
2018-09-12 19:17:15 -0500

    uploaded challenge solution

---
+++ Person UI/Person UI/AppDelegate.swift
@@ -0,0 +1,43 @@
+//
+//  AppDelegate.swift
+//  Person UI
+//  Created by Orrie on 8/29/18.
+//  Copyright © 2018 Tech innovator. All rights reserved.
+
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+        return true
+    }
+
+    func applicationWillResignActive(_ application: UIApplication) {
+        // Sent when the application is about to move from active to inactive state.
+    }
+
+    func applicationDidEnterBackground(_ application: UIApplication) {
+        // Releases shared resources, save user data, invalidate timers, and store information to restore your application in case of termination.
+        // For background execution this method is called instead of applicationWillTerminate: when the user quits.
+    }
+
+    func applicationWillEnterForeground(_ application: UIApplication) {
+        // Called as part of the transition from the background to the active state.
+    }
+
+    func applicationDidBecomeActive(_ application: UIApplication) {
+        // Restart any tasks that were paused (or not yet started) while the application was inactive. If the application was previously in the background, optionally refresh the user interface.
+    }
+
+    func applicationWillTerminate(_ application: UIApplication) {
+        // Called when the application is about to end. Saves the data if appropriate. Also refers to applicationDidEnterBackground:.
+    }
+
+
+}
+
---
+++ Person UI/Person UI/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Person UI
+//  Created by Orrie on 8/29/18.
+//  Copyright © 2018 Tech innovator. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // clear resources that can be recreated.
+    }
+
+
+}
+
